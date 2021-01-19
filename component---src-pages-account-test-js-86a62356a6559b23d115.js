2c54f2eafcaa5771cbf440244ee793fb5ca1234d
cheehau <puahcheehau@gmail.com>
2021-01-19 00:44:02 +0000

    Update main to output generated at afaf0b8

--- component---src-pages-account-test-js-86a62356a6559b23d115.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{TCxs:function(n,t,e){"use strict";e.r(t);var a=e("q1tI"),r=e.n(a);t.default=function(){return r.a.createElement(r.a.Fragment,null,"test")}}}]);
-//# sourceMappingURL=component---src-pages-account-test-js-86a62356a6559b23d115.js.map
